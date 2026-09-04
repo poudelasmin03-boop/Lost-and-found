e99d1c6f1b4d227331b7ecfa9b39058f352c8729
asminpoudel35-debug <asminpoudel35@gmail.com>
2026-09-04 20:46:08 +0545

    gitignore

--- landf/settings.py
+++ landf/settings.py
@@ -4,15 +4,28 @@ Django settings for landf project.
 
 from pathlib import Path
 
+
+
+
+import os
+from dotenv import load_dotenv
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
+
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a%d9h+xg!&vq-=hajpbn)5c7^_16!(by0eq%c5o)@almwwaw9q"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -63,8 +76,12 @@ WSGI_APPLICATION = "landf.wsgi.application"
 # Database
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
