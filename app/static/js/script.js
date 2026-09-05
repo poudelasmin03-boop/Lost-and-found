8e937bc164a56a3d08edd1e8fcde8fafcda73669
asminpoudel35-debug <asminpoudel35@gmail.com>
2026-09-05 15:21:11 +0545

    add js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,20 @@
 AOS.init();
+
+function togglePassword(){
+  console.log("clicked effect");
+  let login = document.getElementById('loginpassword');
+  let span = document.querySelector('.show-pass-btn');
+
+
+  if (login.type === "password"){
+      login.type = "text";
+      span.innerHTML = `<i class="fa-regular fa-eye"></i>`
+      
+  }
+  else{
+      login.type = "password";
+       span.innerHTML = `<i class="fa-regular fa-eye-slash"></i>`
+      
+  }
+
+}
